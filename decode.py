7beaab076a26d1fc0e1584a33053eb7e1502da92
Steven <malvidin@users.noreply.github.com>
2020-12-17 11:42:30 +0100

    Refactor for readability

--- decode.py
+++ decode.py
@@ -1,153 +1,130 @@
 #!/usr/env python
 # -*- coding:utf-8 -*-
+import re
 import sys
+from base64 import b32encode, b32decode
 import random
-
 import argparse
 
 '''
 # author = "QiAnXin_RedDrip"
 # twitter = @RedDrip7
 # create_date = "2020-12-15"
-# update_date = "2020-12-16"
 # Thanks QiAnXin CERT for the discovery of decodeable DGA domains
 # https://mp.weixin.qq.com/s/v-ekPFtVNZG1W7vWjcuVug
+# modified by @malvidin
+# update_date = "2020-12-17"
 '''
 
 
-def Base32Encode(input_string, rt):
-    text = "ph2eifo3n5utg1j8d94qrvbmk0sal76c"
-    text2 = ""
-    num = 0
-    ib = 0
-    for i in range(len(input_string)):
-        iint = input_string[i]
-        b = '0x{0:{1}X}'.format(ord(iint), 2)
-        num |= (int(b, 16) << ib)
-        ib += 8
-        while (ib >= 5):
-            text2 += text[num & 31]
-            num >>= 5  # 将高位的部分右移
-            ib -= 5
-            pass
-        pass
-
-    if (ib > 0):
-        if (rt):
-            pass
-        text2 += text[(num & 31)]
-        pass
-
-    return text2
+def make_trans(text_in, text_out):
+    if isinstance(text_in, bytes):
+        try:
+            trans = bytes.maketrans(text_in, text_out)
+        except:
+            import string
+            trans = string.maketrans(text_in, text_out)
+    else:
+        try:
+            trans = str.maketrans(text_in, text_out)
+        except:
+            import string
+            trans = string.maketrans(text_in, text_out)
+    return trans
 
 
-def Base32Decode(b32string):
-    text = "ph2eifo3n5utg1j8d94qrvbmk0sal76c"
-    restring = ""
-    datalen = len(b32string) // 8 * 5
-    num = 0
-    ib = 0
-    if len(b32string) < 3:
-        restring = chr(text.find(b32string[0]) | text.find(b32string[1]) << 5 & 255)
-        return restring
+def ensure_str(input_string):
+    if isinstance(input_string, str):
+        return input_string
+    if isinstance(input_string, bytes):
+        return input_string.decode()
+    if isinstance(input_string, unicode):
+        return input_string.encode()
+    return str(input_string)
 
-    k = text.find(b32string[0]) | (text.find(b32string[1]) << 5)
-    j = 10
-    index = 2
-    for i in range(datalen):
-        restring += chr(k & 255)
-        k = k >> 8
-        j -= 8
-        while (j < 8 and index < len(b32string)):
-            k |= (text.find(b32string[index]) << j)
-            index += 1
-            j += 5
 
-    return restring
+def custom_base32encode(input_string):
+    text = b'ph2eifo3n5utg1j8d94qrvbmk0sal76c'
+    trans = make_trans(b'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567', text)
+    # reverse, custom b32encode, reverse
+    b32_string = b32encode(input_string[::-1].encode())[::-1]
+    ret_string = b32_string.translate(trans).decode()
+    return ret_string
+
+
+def custom_base32decode(input_string):
+    text = b'ph2eifo3n5utg1j8d94qrvbmk0sal76c'
+    trans = make_trans(text, b'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567')
+    # reverse, custom b32decode, reverse
+    b32_string = input_string.translate(trans)
+    ret_string = b32decode(b32_string[::-1].encode())[::-1].decode()
+    return ret_string
 
 
 '''OrionImprovementBusinessLayer.CryptoHelper.Base64Decode'''
 
 
-def Encode(input_string):
-    text = "rq3gsalt6u1iyfzop572d49bnx8cvmkewhj"
-    text2 = "0_-."
-    text3 = ""
-    for i in range(len(input_string)):
-        ch = input_string[i]
-        tx_index = -1
-        tx2_index = -1
-        if ch in text2:
-            tx2_index = text2.find(ch)
-            text3 = text3 + text2[0] + text[tx2_index + (random.randint(0, 8) % (len(text) // len(text2))) * len(text2)]
-        else:
-            tx_index = text.find(ch)
-            text3 = text3 + text[(tx_index + 4) % len(text)]
+def encode_sub_cipher(input_string):
+    text = 'rq3gsalt6u1iyfzop572d49bnx8cvmkewhj'
+    text_spec = '0_-.'
+    trans = make_trans(text, text[4:] + text[:4])
+    trans_string = input_string.translate(trans)
+    # Use # to track the special substitutions
+    re_spec = '([{}])'.format(re.escape(text_spec))
+    trans_string = re.sub(re_spec, r'#\1', trans_string)
+    # make the substitutions based on the text replacement string
+    spec_choices = {k:text[i::len(text_spec)] for i,k in enumerate(text_spec)}
+    while '#' in trans_string:
+        idx = trans_string.find('#')
+        trans_char = trans_string[idx+1]
+        trans_string = trans_string[:idx] + '{}{}'.format(text_spec[0], random.choice(spec_choices[trans_char])) + trans_string[idx+2:]
 
-    return text3
+    return trans_string
 
 
 '''OrionImprovementBusinessLayer.CryptoHelper.Base64Decode-decode'''
 
 
-def Decode(input_string):
-    text = "rq3gsalt6u1iyfzop572d49bnx8cvmkewhj"
-    text2 = "0_-."
-    retstring = ""
-    flag = False
-    for i in range(len(input_string)):
-        ch = input_string[i]
-        tx_index = -1
-        tx2_index = -1
-        if flag:
-            t1i = text.find(ch)
-            x = t1i - ((random.randint(0, 8) % (len(text) // len(text2))) * len(text2))
-            retstring = retstring + text2[x % len(text2)]
-            flag = False
-            continue
-        if ch in text2:
-            tx2_index = text2.find(ch)
-            flag = True
-            pass
-        else:
-            tx_index = text.find(ch)
-            oindex = tx_index - 4
-            retstring = retstring + text[oindex % len(text)]
+def decode_subs_cipher(input_string):
+    text = 'rq3gsalt6u1iyfzop572d49bnx8cvmkewhj'
+    text_spec = '0_-.'
+    trans = make_trans(text[4:] + text[:4], text)
 
-        pass
-    return retstring
+    for i, ch in enumerate(input_string):
+        if ch in text_spec:
+            spec_idx = text.find(input_string[i+1]) % len(text_spec)
+            # Since we're walking through each character, the string length must not change
+            input_string = input_string[:i] + '#' + text_spec[spec_idx] + input_string[i+2:]
+    input_string = input_string.replace('#', '')
+    trans_string = input_string.translate(trans)
+
+    return trans_string
 
 
-# print (Encode("qingmei-inc.com"))
-# print (Decode("aovthro08ove0ge2h"))
-# print (Base32Decode("9tslbqv1ftss4r01eqtobmv1"))
+def decode_dga(input_string):
+    data = input_string.split('.', maxsplit=1)[0]
+    system_guid, single_char, dn_str_lower = ('',) * 3
+    if len(data) >= 16:
+        system_guid = data[:15]
+        single_char = data[16]
+        if '0' in data[16:]:
+            in_string = data[16:]
+            try:
+                if "00" in in_string:
+                    in_string = in_string[2:]
+                    dn_str_lower = custom_base32decode(in_string)
+                else:
+                    dn_str_lower = decode_subs_cipher(in_string)
+            except:
+                pass
+    return system_guid, single_char, dn_str_lower
 
-'''
-  1fik67gkncg86q6daovthro0love0oe2.appsync-api.us-west-2.avsvmcloud.com
-  this.guid  --> 1fik67gkncg86q6
-  单个字符：  d
-  域： qingmei-inc.com   --- > aovthro0love0oe2h  但是由于取的是this.dnStrLower而不是this.dnStr，会吞掉一部分字符,所以最终"h"被吞掉了
-'''
 
-# # 在有域的主机中，走GetPreviousString()模式, 拼接方式： CreateSecureString(this.guid, true) + 一个字符 + this.dnStrLower + ".appsync-api.us-west-2.avsvmcloud.com"
-# for line in sys.stdin:
-#     data = line.rstrip().split(".")[0]
-#     if len(data) < 16:
-#         continue
-#     string = data[16:]  # 前十六字节： this.guid + 一个字符
-#     if "0" not in string:  # 如果没有0，说明不是OrionImprovementBusinessLayer.CryptoHelper.Base64Encode和OrionImprovementBusinessLayer.CryptoHelper.Base64Decode，暂时解不开
-#         continue
-#     try:
-#         if "00" in string:
-#             string = string[2:]
-#             comp = Base32Decode(string)
-#         else:
-#             comp = Decode(string)
-#
-#         print ("%s,%s" % (line.rstrip(), comp))
-#     except:
-#         pass
-#
+assert decode_subs_cipher('aovthro08ove0ge2h') == 'qingmei-inc.com'
+assert decode_subs_cipher(encode_sub_cipher('qingmei-inc.com')) == 'qingmei-inc.com'
+assert custom_base32encode('qingmei-inc.com') == '9tslbqv1ftss4r01eqtobmv1'
+assert custom_base32decode('9tslbqv1ftss4r01eqtobmv1') == 'qingmei-inc.com'
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -159,21 +136,9 @@ def main():
     in_file = args.input
     out_file = args.output
     for line in in_file:
-        data = line.rstrip().split(".")[0]
-        if len(data) < 16:
-            continue
-        in_string = data[16:]  # 前十六字节： this.guid + 一个字符
-        if "0" not in in_string :  # 如果没有0，说明不是OrionImprovementBusinessLayer.CryptoHelper.Base64Encode和OrionImprovementBusinessLayer.CryptoHelper.Base64Decode，暂时解不开
-            continue
-        try:
-            if "00" in in_string :
-                in_string  = in_string [2:]
-                comp = Base32Decode(in_string)
-            else:
-                comp = Decode(in_string)
-            out_file.write('{0}, {1}\n'.format(line.rstrip(), comp))
-        except:
-            pass
+        line = line.rstrip()
+        system_guid, single_char, dn_str_lower = decode_dga(line)
+        out_file.write(','.join([line, system_guid, single_char, dn_str_lower]) + '\n')
 
 
 if __name__ == '__main__':
