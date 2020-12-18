3699bef5010bb8530c9eacc6d00462079d3bc2f7
Steven <malvidin@users.noreply.github.com>
2020-12-19 00:57:50 +0100

    Join Records
    Added brute force joining of separated queries, for both the substitution and modified base32 decoding.

--- decode.py
+++ decode.py
@@ -4,6 +4,7 @@ import re
 import sys
 import random
 import argparse
+from itertools import permutations
 
 '''
 # author = "QiAnXin_RedDrip"
@@ -12,7 +13,7 @@ import argparse
 # Thanks QiAnXin CERT for the discovery of decodeable DGA domains
 # https://mp.weixin.qq.com/s/v-ekPFtVNZG1W7vWjcuVug
 # modified by @malvidin
-# update_date = "2020-12-17"
+# update_date = "2020-12-19"
 '''
 
 
@@ -147,13 +148,13 @@ def decode_dga(input_string, prev_strings=None):
     if prev_strings is None:
         prev_strings = []
     data = input_string.split('.', maxsplit=1)[0]
-    system_guid, dn_str_lower = ('',) * 2
+    system_guid, dn_str_lower, decode_info = ('',) * 3
     if len(data) >= 16:
         try:
-            system_guid = decode_guid(data[:16])
+            system_guid = decode_guid(data)[:16]
         except:
             pass
-        encoded_string = data[16:]
+        encoded_string = data[16:].rstrip('0')
 
         if '0' in data[16:]:
             try:
@@ -165,20 +166,24 @@ def decode_dga(input_string, prev_strings=None):
                         dn_str_lower_test = custom_base32decode(encoded_string + prev_string)
                         if all(ord(char) < 128 for char in dn_str_lower_test):
                             dn_str_lower = dn_str_lower_test
+                            decode_info = 'custom_base32'
                             break
                     if dn_str_lower is None:
                         dn_str_lower = custom_base32decode(encoded_string)
+                        decode_info = 'custom_base32'
                 else:
                     # Substitution Cipher
                     # This will incorrectly decode some continuation characters from base32 encoding that contain '0'
-                    dn_str_lower = decode_subs_cipher(encoded_string.rstrip('0'))
+                    dn_str_lower = decode_subs_cipher(encoded_string)
+                    decode_info = 'subs_cipher'
             except:
-                dn_str_lower = '(decode failed)'
+                decode_info = 'decode failed'
         else:
             # These strings be from a domain that 16+ characters long, or continuation characters from base32 encoding
-            dn_str_lower = decode_subs_cipher(encoded_string) + ' (no dot)'
+            dn_str_lower = decode_subs_cipher(encoded_string)
+            decode_info = 'subs_cipher (no dot)'
 
-    return system_guid, dn_str_lower
+    return system_guid, encoded_string, dn_str_lower, decode_info
 
 assert decode_subs_cipher('aovthro08ove0ge2h') == 'qingmei-inc.com'
 assert decode_subs_cipher(encode_sub_cipher('qingmei-inc.com')) == 'qingmei-inc.com'
@@ -202,19 +207,38 @@ def main():
     summary_dict = {}
     for line in in_file:
         line = line.rstrip()
-        system_guid, dn_str_lower = decode_dga(line)
-        out_file.write(','.join([line, system_guid, dn_str_lower]) + '\n')
+        system_guid, encoded_string, dn_str_lower, decode_info = decode_dga(line)
+        out_file.write(','.join([line, system_guid, dn_str_lower, decode_info]) + '\n')
         
         if system_guid in summary_dict:
-            if dn_str_lower not in summary_dict[system_guid]:
-                summary_dict[system_guid]
+            summary_dict[system_guid]['dn_str_lower'].add(dn_str_lower)
+            summary_dict[system_guid]['decode_info'].add(decode_info)
+            summary_dict[system_guid]['encoded_string'].add(encoded_string)
         else:
-            summary_dict[system_guid] = [dn_str_lower]
-        if summary_dict:
-            out_file.write('\nSummary by GUID:\n')
-            for guid, dn_list in summary_dict.items():
-                for li in dn_list:
-                    out_file.write(','.join([guid, li]) + '\n')
+            summary_dict[system_guid] = {
+                'dn_str_lower': {dn_str_lower}, 
+                'decode_info': {decode_info},
+                'encoded_string': {encoded_string},
+                }
+    if summary_dict:
+        out_file.write('\nSummary by GUID:\n')
+        for guid, summ_info in summary_dict.items():
+            dn_str_list = None
+            if 'custom_base32' in summ_info['decode_info']:
+                dn_str_list = []
+                try:
+                    for p in permutations(summ_info['encoded_string']):
+                        dn_str_lower_test = custom_base32decode(''.join(p))
+                        if all(ord(char) < 128 for char in dn_str_lower_test):
+                            dn_str_list.append(dn_str_lower_test)
+                except:
+                    pass
+            if dn_str_list:
+                out_file.write(','.join([guid,] + dn_str_list) + '\n')
+            else:
+                for p in permutations(summ_info['dn_str_lower']):                    
+                    out_file.write(','.join([guid, ''.join(p)]) + '\n')
+            out_file.write('\n')
 
 
 if __name__ == '__main__':
