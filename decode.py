ea3c87a5d4590091e66e88fedc01f345c4f6e122
Steven <malvidin@users.noreply.github.com>
2020-12-18 13:46:32 +0100

    Variable Name Cleanup
    Added comments about when some strings will be incorrectly encoded. 
    
    Added residual status of the base32 decoding for further processing of any continuation queries.

--- decode.py
+++ decode.py
@@ -2,7 +2,6 @@
 # -*- coding:utf-8 -*-
 import re
 import sys
-from base64 import b32encode, b32decode
 import random
 import argparse
 
@@ -43,30 +42,29 @@ def ensure_str(input_string):
     return str(input_string)
 
 
-def custom_base32encode(input_string, rt):
+def custom_base32encode(input_string, rt=True):
     text = 'ph2eifo3n5utg1j8d94qrvbmk0sal76c'
     ret_string= ''
-    num = 0
-    ib = 0
-    for ch in input_string:
-        num |= ord(ch) << ib
-        ib += 8
-        while ib >= 5:
-            ret_string += text[num & 0b11111]
-            num >>= 5  #将高位的部分右移
-            ib -= 5
-    if ib > 0:
-        if rt:
-            ret_string += text[num & 0b11111]
-    return ret_string
-
-
-def custom_base32decode(input_string):
-    text = 'ph2eifo3n5utg1j8d94qrvbmk0sal76c'
-    ret_string = ''
 
     bits_on_stack = 0
     bit_stack = 0
+    for ch in input_string:
+        bit_stack |= ord(ch) << bits_on_stack
+        bits_on_stack += 8
+        while bits_on_stack >= 5:
+            ret_string += text[bit_stack & 0b11111]
+            bit_stack >>= 5  #将高位的部分右移
+            bits_on_stack -= 5
+    if bits_on_stack > 0:
+        if rt:
+            ret_string += text[bit_stack & 0b11111]
+    return ret_string
+
+
+def custom_base32decode(input_string, rt=True, bits_on_stack=0, bit_stack=0):
+    text = 'ph2eifo3n5utg1j8d94qrvbmk0sal76c'
+    ret_string = ''
+
     for ch in input_string:
         bit_stack |= text.find(ch) << bits_on_stack
         bits_on_stack += 5
@@ -74,6 +72,9 @@ def custom_base32decode(input_string):
             ret_string += chr(bit_stack & 255)
             bit_stack >>= 8
             bits_on_stack -= 8
+    if bits_on_stack > 0 and bit_stack > 0:
+        if rt:
+            ret_string += ' (0b{:06b}, {})'.format(bit_stack & 255, bits_on_stack)
     return ret_string
 
 
@@ -117,26 +118,40 @@ def decode_subs_cipher(input_string):
     return trans_string
 
 
-def decode_dga(input_string):
+def decode_dga(input_string, prev_strings=None):
+    if prev_strings is None:
+        prev_strings = []
     data = input_string.split('.', maxsplit=1)[0]
     system_guid, single_char, dn_str_lower = ('',) * 3
     if len(data) >= 16:
         system_guid = data[:15]
         single_char = data[16]
-        in_string = data[16:]
+        encoded_string = data[16:]
+
         if '0' in data[16:]:
             try:
-                if in_string.startswith('00'):
-                    in_string = in_string[2:]
-                    dn_str_lower = custom_base32decode(in_string)
+                dn_str_lower = None
+                if encoded_string.startswith('00'):
+                    # Custom Base32 Encoding
+                    encoded_string = encoded_string[2:]
+                    for prev_string in prev_strings:
+                        dn_str_lower_test = custom_base32decode(encoded_string + prev_string)
+                        if all(ord(char) < 128 for char in dn_str_lower_test):
+                            dn_str_lower = dn_str_lower_test
+                            break
+                    if dn_str_lower is None:
+                        dn_str_lower = custom_base32decode(encoded_string)
                 else:
-                    dn_str_lower = decode_subs_cipher(in_string)
+                    # Substitution Cipher
+                    # This will incorrectly decode some continuation characters from base32 encoding that contain '0'
+                    dn_str_lower = decode_subs_cipher(encoded_string.rstrip('0'))
             except:
                 dn_str_lower = '(decode failed)'
         else:
-            dn_str_lower = decode_subs_cipher(in_string) + ' (no dot)'
-    return system_guid, single_char, dn_str_lower
+            # These strings be from a domain that 16+ characters long, or continuation characters from base32 encoding
+            dn_str_lower = decode_subs_cipher(encoded_string) + ' (no dot)'
 
+    return system_guid, single_char, dn_str_lower
 
 assert decode_subs_cipher('aovthro08ove0ge2h') == 'qingmei-inc.com'
 assert decode_subs_cipher(encode_sub_cipher('qingmei-inc.com')) == 'qingmei-inc.com'
